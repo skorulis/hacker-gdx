1b8c3ef3f94bca560b08fe958417a1d2209def4d
Alexander Skorulis <skorulis@gmail.com>
2014-04-08 13:14:01 +1000

    Added basic skin files so I can build a basic UI.

--- hacker2/src/skorulis/hacker/ui/GameUI.java
+++ hacker2/src/skorulis/hacker/ui/GameUI.java
@@ -1,5 +1,19 @@
 package skorulis.hacker.ui;
 
-public class GameUI {
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
+public class GameUI extends Group {
+
+	public TextButton actionButton;
+	
+	public GameUI(Skin skin) {
+		actionButton = new TextButton("Action", skin);
+		actionButton.setWidth(100);
+		actionButton.setHeight(100);
+		
+		addActor(actionButton);
+	}
+	
 }
--- hacker2/src/skorulis/hacker/ui/UIManager.java
+++ hacker2/src/skorulis/hacker/ui/UIManager.java
@@ -1,14 +1,21 @@
 package skorulis.hacker.ui;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class UIManager {
 
-	public Stage stage;
+	private Stage stage;
+	private GameUI gameUI;
+	private Skin skin;
 	
 	public UIManager() {
 		stage = new Stage(new ScreenViewport());
+		skin = new Skin(Gdx.files.internal("data/skin/uiskin.json"));
+		gameUI = new GameUI(skin);
+		stage.addActor(gameUI);
 	}
 	
 	public void render() {
